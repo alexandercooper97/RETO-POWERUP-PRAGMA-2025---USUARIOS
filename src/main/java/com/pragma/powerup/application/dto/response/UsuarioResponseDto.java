3455ca57a408f4dd46ab50247a01b039b4db0dd8
Alexander Cooper <130676645+alexandercooper97@users.noreply.github.com>
2025-04-17 10:04:13 -0500

    HU 01

---
+++ src/main/java/com/pragma/powerup/application/dto/response/UsuarioResponseDto.java
@@ -0,0 +1,19 @@
+package main.javacom.pragma.powerup.application.dto.response;
+
+import main.java.com.pragma.powerup.domain.model.Rol;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioResponseDto {
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String documentoDeIdentidad;
+    private  String celular;
+    private String correo;
+    private String clave;
+
+    private Rol rol;
+}
