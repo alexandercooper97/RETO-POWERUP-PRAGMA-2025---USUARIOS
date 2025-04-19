e057c6c37b42709a5f0b66f6e60330df4e215a35
Alexander Cooper <130676645+alexandercooper97@users.noreply.github.com>
2025-04-18 19:43:12 -0500

    HU 01

---
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/mapper/IRolEntityMapper.java
@@ -0,0 +1,20 @@
+package main.java.com.pragma.powerup.infrastructure.out.jpa.mapper;
+
+import main.java.com.pragma.powerup.domain.model.Rol;
+import main.java.com.pragma.powerup.infrastructure.out.jpa.entity.RolEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface IRolEntityMapper {
+
+    RolEntity toEntity(Rol rol);
+
+    Rol toRolModel(RolEntity rolEntity);
+
+    List<Rol> toRolModelList(List<RolEntity> rolEntityList);
+}
